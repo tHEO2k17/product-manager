8b1bacacdb3c11d8bf48bd9c7d68b25676102f69
tHEO2k17 <tpee935@gmail.com>
2021-03-01 16:16:01 +0000

    pricing history implemented

--- src/presentation/views/products/components/product.history.js
+++ src/presentation/views/products/components/product.history.js
@@ -3,7 +3,9 @@ import {
     CardBody,
     Row,
     Col,
-    Button
+    Button,
+    UncontrolledCollapse,
+    ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText
 } from "reactstrap"
 
 import { useSelector } from 'react-redux';
@@ -14,27 +16,47 @@ const ProductHistory = () => {
 
     return <div>
         <h4>Products</h4>
-        {
-            productList.map(item => (
-                <Card className="my-2">
-                    <CardBody>
-                        <Row>
-                            <Col>
-                                <h5>
-                                    {item.name}
-                                    <span className="text-muted float-right fs-6">
-                                        <small>Latest Price: GHS {item.prices[0].price}</small>
-                                    </span>
-                                </h5>
-                            </Col>
-                            <Col className="text-right" sm="auto">
-                                <Button color="link">Edit</Button>
-                                <Button color="link">Delete</Button>
-                            </Col>
-                        </Row>
-                    </CardBody>
-                </Card>
-            ))
+        {productList.map(item => (
+            <Card className="my-2" key={item.id}>
+                <CardBody >
+                    <Row>
+                        <Col>
+                            <h5>
+                                {item.name}
+                                <span className="text-muted float-right fs-6">
+                                    <small>Latest Price: GHS {item.prices[0].price}</small>
+                                </span>
+                            </h5>
+                        </Col>
+                        <Col className="text-right" sm="auto">
+                            <Button color="link" id="toggler">History</Button>
+                            <Button color="link">Edit</Button>
+                            <Button color="link">Delete</Button>
+                        </Col>
+                    </Row>
+                </CardBody>
+                <UncontrolledCollapse toggler="#toggler">
+                    <ListGroup horizontal className="mx-4 my-3">
+                        {
+                            item.prices.map((priceItem) => (
+                                <ListGroupItem key={priceItem.id}>
+                                    <ListGroupItemHeading>Ghs {priceItem.price}</ListGroupItemHeading>
+                                    <ListGroupItemText>
+                                        {
+                                            new Intl.DateTimeFormat("en-GB", {
+                                                year: "numeric",
+                                                month: "long",
+                                                day: "2-digit"
+                                            }).format(new Date(priceItem.date))
+                                        }
+                                    </ListGroupItemText>
+                                </ListGroupItem>
+                            ))
+                        }
+                    </ListGroup>
+                </UncontrolledCollapse>
+            </Card>
+        ))
         }
 
 
